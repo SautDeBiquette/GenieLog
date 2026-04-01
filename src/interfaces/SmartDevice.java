6383605da3d76278a2359c31420fdd00e536dbe4
vrossi <valentin.cs.rossi@gmail.com>
2026-04-01 15:14:59 +0200

    yakak

--- src/interfaces/SmartDevice.java
+++ src/interfaces/SmartDevice.java
@@ -1,4 +1,11 @@
 package interfaces;
 
+import java.sql.Time;
+
 public interface SmartDevice {
+    void turnOn();
+    void turnOff();
+    String notifyUser();
+    String schedule(Time timeScheduled);
+    String report();
 }
