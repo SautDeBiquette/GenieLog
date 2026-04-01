26867ac08599bd706765787a5982b3e8f8e07531
vrossi <valentin.cs.rossi@gmail.com>
2026-04-01 15:46:40 +0200

    yakak

--- src/factories/DeviceFactory.java
+++ src/factories/DeviceFactory.java
@@ -5,33 +5,5 @@ import interfaces.SmartDevice;
 import java.sql.Time;
 
 public abstract class DeviceFactory {
-    private int serialNumber;
-    private String label;
-    private Time schedule;
-
     public abstract SmartDevice createSmartDevice();
-
-    public int getSerialNumber() {
-        return serialNumber;
-    }
-
-    public void setSerialNumber(int serialNumber) {
-        this.serialNumber = serialNumber;
-    }
-
-    public String getLabel() {
-        return label;
-    }
-
-    public void setLabel(String label) {
-        this.label = label;
-    }
-
-    public Time getSchedule() {
-        return schedule;
-    }
-
-    public void setSchedule(Time schedule) {
-        this.schedule = schedule;
-    }
 }
